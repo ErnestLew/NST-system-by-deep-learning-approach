f01c6900fe9089f267054c1fecaa85cea1699920
Lew Jun Xian <68409261+ErnestLew@users.noreply.github.com>
2022-04-02 00:41:00 +0800

    Delete final-version-main directory

--- final-version-main/apps/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from PIL import Image
-
-def app():
-    st.title("Neural Style Transfer for Wedding Photo")
-    image = Image.open('imgs/lake.jpeg')
-    st.image(image, caption='Welcome to my webapp!', use_column_width=True)
-
-   
